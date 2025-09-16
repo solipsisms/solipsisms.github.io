4848f2c3423a9c99e339e34578b8639e03660777
sophie <120543057+solipsisms@users.noreply.github.com>
2025-09-15 23:12:58 -0500

    configure deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,5 +13,6 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [mdx(), react()]
+  integrations: [mdx(), react()],
+  site: 'https://solipsisms.github.io',
 });
